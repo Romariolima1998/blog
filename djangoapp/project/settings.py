0bab9b6c140f2f378946ce8bc55ea75f6671f15b
romario lima <romariolima1006@gmail.com>
2024-03-05 11:43:32 -0300

    ALLOWED_HOST harded code

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'change-me')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
-ALLOWED_HOSTS = [
-    h.strip() for h in os.getenv('ALLOWED_HOTS', '').split(',') if h.strip()
+ALLOWED_HOSTS = [ '34.16.199.42', '127.0.0.1', 'localhost'
+   # h.strip() for h in os.getenv('ALLOWED_HOTS', '').split(',') if h.strip()
 ]
 
 
