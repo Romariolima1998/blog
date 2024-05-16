099feb83c41ca386f79b93f2278a395b2042db61
romario lima <romariolima1006@gmail.com>
2024-05-16 07:29:26 -0300

    comentado load_dotenv de project.wsgi

--- project/wsgi.py
+++ project/wsgi.py
@@ -12,12 +12,12 @@ from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR.parent / 'dotenv_files' / '.env', override=True)
+# load_dotenv(BASE_DIR.parent / 'dotenv_files' / '.env', override=True)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
